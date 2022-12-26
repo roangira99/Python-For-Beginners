54dbe2e838c275ff16df7502d10e2aeb30fc89f9
roangira <angirarobert7@gmil.com>
2022-12-26 19:05:19 +0300

    Learnt about working with input and output in Python

---
+++ input_and_output.py
@@ -0,0 +1,24 @@
+from datetime import date
+import sys
+
+# using datetime module
+# print("Today's date is: " + str(date.today()))
+
+# using sys module
+# print(sys.argv)
+# print(sys.argv[0]) # program name
+# # print(sys.argv[1]) # first arg
+
+# user input
+# print("Welcome to the greeter program")
+# name = input("Enter your name: ")
+# print("Greetings " + name)
+
+# simple calculator program
+print("calculator program")
+first_number = int(input("First number:"))
+second_number = int(input("Second number:"))
+print(first_number + second_number)
+
+sum = "1" + 2
+print(sum)
