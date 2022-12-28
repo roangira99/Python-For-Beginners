4927670a860b3c9eeb25a6b16ec159bae4ed37e2
roangira <angirarobert7@gmil.com>
2022-12-28 18:51:00 +0300

    if statements and operators in python

---
+++ if_statements.py
@@ -0,0 +1,28 @@
+# a = 256
+# b = 256
+
+# # test expression
+# if a < b:
+#     # statement to be run
+#     print("a is less than b")
+# elif a == b:
+#     print("a is equal to b")
+# elif a > b:
+#     print("a is greater than b")
+# else:
+#     print("a equal to b")
+
+# nested conditional logic
+a = 155
+b = 76
+c = 91
+
+if a > b:
+    if b > c:
+        print ("a is greater than b and b is greater than c")
+    else:
+        print("a is greater than b and b less than c")
+elif a == b:
+    print("a is equal to b")
+else: 
+    print('a is less than b')
